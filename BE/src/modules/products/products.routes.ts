5ba8d84b0318dcbb56c681187ebe25c53ef982f3
Darko Stojanovski <darko.stojanovski.mk@gmail.com>
2026-03-25 13:11:14 +0100

    added token auth

--- BE/src/modules/products/products.routes.ts
+++ BE/src/modules/products/products.routes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import productsController from "./products.controller.js";
+import { verifyToken } from "../../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/api/products", productsController.getAll);
-router.post("/api/products", productsController.create);
-router.put("/api/products/:id", productsController.update);
-router.delete("/api/products/:id", productsController.delete);
+router.get("/api/products", verifyToken, productsController.getAll);
+router.post("/api/products", verifyToken, productsController.create);
+router.put("/api/products/:id", verifyToken, productsController.update);
+router.delete("/api/products/:id", verifyToken, productsController.delete);
 
 export default router;
--- FE/src/routes/PrivateRoutes.tsx
+++ FE/src/routes/PrivateRoutes.tsx
@@ -3,7 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Products from "../pages/products/products";
 
 export default function PrivateRoutes() {
-  console.log("here");
+
   return (
     <Routes>
       <Route path="/" element={<Products />} />
